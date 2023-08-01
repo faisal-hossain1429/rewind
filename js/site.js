cc06c2ff16c2f385be619833a88e4434bef90e61
faisal-hossain1429 <121063890+faisal-hossain1429@users.noreply.github.com>
2023-08-01 19:16:13 -0400

    Completed Full project
    git link not live on page

--- js/site.js
+++ js/site.js
@@ -1,5 +1,42 @@
-//Call function hellow world
+//Collect values from page (string)
+//controller function
+    function getValue(){
 
-function HelloWorld(){
-    alert("hello world");
-}
+        document.getElementById("alert").classList.add("invisible");
+
+
+        let userString = document.getElementById("reverseMe").value;
+        let revString = reverseString(userString); // allows next function to see the private variable
+        displayString(revString);
+    }
+
+//reverse the string
+//logic function
+
+    function reverseString(userString){
+
+        let revString = "";
+
+        //reverse a string using a for loop
+
+        for (let index =(userString.length - 1); index >= 0; index--) {
+            
+            revString += userString[index];
+
+        }
+
+        return revString;
+
+    }
+
+//display message to the user
+//view function
+
+    function displayString(revString){
+
+        //write message, then open alert
+
+        document.getElementById("msg").innerHTML = `Your string reversed is ${revString}`;  
+        document.getElementById("alert").classList.remove("invisible");
+        
+    }
